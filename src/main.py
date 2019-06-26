ad4ebd06264294d76bc96b33b39dca3f4e954224
João Henrique Xavier <jhcxavier@gmail.com>
2019-06-26 23:29:19 +0000

    updating main.py

--- src/main.py
+++ src/main.py
@@ -25,52 +25,6 @@ def sitemap():
     return generate_sitemap(app)
 
 
-# @app.route('/person/<int:todo_id>', methods=['PUT', 'GET', 'DELETE'])
-# def get_single_person(todo_id):
-#     """
-#     Single person
-#     """
-
-#     # PUT request
-#     if request.method == 'PUT':
-#         body = request.get_json()
-#         if body is None:
-#             raise APIException("You need to specify the request body as a json object", status_code=400)
-
-#         todo11 = Todo1.query.get(todo_id)
-#         if todo11 is None:
-#             raise APIException('User not found', status_code=404)
-
-#         if "username" in body:
-#             todo11.username = body["username"]
-#         if "email" in body:
-#             todo11.email = body["email"]
-#         db.session.commit()
-
-#         return jsonify(todo11.serialize()), 200
-
-#     # GET request
-#     if request.method == 'GET':
-#         todo11 = Todo1.query.get(todo_id)
-#         if todo11 is None:
-#             raise APIException('User not found', status_code=404)
-#         return jsonify(todo11.serialize()), 200
-
-#     # DELETE request
-#     if request.method == 'DELETE':
-#         todo11 = Person.query.get(todo_id)
-#         if todo11 is None:
-#             raise APIException('User not found', status_code=404)
-#         db.session.delete(todo11)
-#         return "ok", 200
-
-#     return "Invalid Method", 404
-
-
-# if __name__ == '__main__':
-#     PORT = int(os.environ.get('PORT', 3000))
-#     app.run(host='0.0.0.0', port=PORT)
-
 
 @app.route('/todolist', methods=['POST', 'GET'])
 def handle_todo():
